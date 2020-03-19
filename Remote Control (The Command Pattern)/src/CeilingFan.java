ac232976e248d2790c321d30c4f2728c83eaf6bd
Nihat <nihatallahverdiyev@gmail.com>
2020-03-19 23:49:49 +0100

    Modify implementation of CeilingFan to add speed

--- Remote Control (The Command Pattern)/src/CeilingFan.java
+++ Remote Control (The Command Pattern)/src/CeilingFan.java
@@ -1,14 +1,30 @@
 public class CeilingFan {
-    String roomName;
+    String location;
+    public static final int HIGH = 3;
+    public static final int MEDIUM = 2;
+    public static final int LOW = 1;
+    public static final int OFF = 0;
+    int speed;
 
     public CeilingFan(String roomName) {
-        this.roomName = roomName;
+        this.location = roomName;
+        speed = OFF;
     }
 
-    public void on(){
-        System.out.println(roomName +" ceiling fan is on");
+    public void high(){
+        speed = HIGH;
+    }
+    public void medium(){
+        speed = MEDIUM;
+    }
+    public void low(){
+        speed = LOW;
     }
     public void off(){
-        System.out.println(roomName +" ceiling fan is off");
+        speed = OFF;
+    }
+
+    public int getSpeed() {
+        return speed;
     }
 }
